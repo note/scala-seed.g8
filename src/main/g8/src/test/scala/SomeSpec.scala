dfbcbadeb4c10ce08f88295b9091d0fd1ebc4029
Michał Sitko <pierwszy1@gmail.com>
2020-02-11 12:00:06 +0100

    test fixes

--- src/main/g8/src/test/scala/SomeSpec.scala
+++ src/main/g8/src/test/scala/SomeSpec.scala
@@ -1,9 +1,9 @@
 package example
 
 import org.scalactic.TypeCheckedTripleEquals
-import org.scalatest.{Matchers, WordSpec}
+import org.scalatest.wordspec.AnyWordSpec
 
-class SomeSpec extends WordSpec with Matchers with TypeCheckedTripleEquals {
+class SomeSpec extends AnyWordSpec with TypeCheckedTripleEquals {
 	"2 + 2" should {
 		"equal 4" in {
 			assert((2 + 2) === 4)
