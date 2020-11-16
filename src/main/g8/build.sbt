e6623211e06d3c7deed026390e69e51773587271
Michal Sitko <pierwszy1@gmail.com>
2020-11-16 21:52:41 +0100

    Fix build

--- src/main/g8/build.sbt
+++ src/main/g8/build.sbt
@@ -3,5 +3,5 @@ import Common._
 lazy val root = (project in file("."))
   .commonSettings("$name$", "$version$")
   .settings(
-    libraryDependencies ++= Dependencies.all
+    libraryDependencies ++= Dependencies.compileDeps ++ Dependencies.testDeps
   )
--- src/main/g8/project/Common.scala
+++ src/main/g8/project/Common.scala
@@ -1,6 +1,6 @@
 import com.softwaremill.SbtSoftwareMillCommon.autoImport.commonSmlBuildSettings
 import org.scalafmt.sbt.ScalafmtPlugin.autoImport.scalafmtOnCompile
-import sbt.Keys.{name, organization, scalaVersion, testFrameworks, version}
+import sbt.Keys._
 import sbt.{Project, TestFramework}
 
 object Common {
