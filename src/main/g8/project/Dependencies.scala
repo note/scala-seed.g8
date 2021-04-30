b9efb4dd713dc2b479b57758dc36086b32c57f44
Michal Sitko <pierwszy1@gmail.com>
2021-04-30 07:12:27 +0200

    scalafmt 3.0.0-RC2

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -1,8 +1,8 @@
 import sbt._
 
 object Dependencies {
-	lazy val cats  = "org.typelevel" %% "cats-core" % "2.5.0"
-	lazy val munit = "org.scalameta" %% "munit" % "0.7.23" % Test
+	lazy val cats  = "org.typelevel" %% "cats-core" % "2.6.0"
+	lazy val munit = "org.scalameta" %% "munit" % "0.7.25" % Test
 
 	lazy val compileDeps = Seq(cats)
 	lazy val testDeps    = Seq(munit)
