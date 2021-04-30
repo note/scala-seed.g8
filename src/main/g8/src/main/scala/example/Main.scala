547778b72280969b91f111eee02946c7da171e36
Michal Sitko <pierwszy1@gmail.com>
2021-04-30 07:44:07 +0200

    Scalafmt reproducer

--- src/main/g8/src/main/scala/example/Main.scala
+++ src/main/g8/src/main/scala/example/Main.scala
@@ -1,6 +1,14 @@
 package example
 
+import scala.compiletime.erasedValue
+import scala.compiletime.ops.int._
+
 @main
 def hello(): Unit =
   println("Hello world!")
+
+inline def repro[T <: Int with Singleton, U <: Int with Singleton]: Int = 
+  inline erasedValue[T] match
+    case _: 5 => 5
+    case _    => 0
 
