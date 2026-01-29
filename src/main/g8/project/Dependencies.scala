b02c2485f614181ddb2250cce1ed822849b5e7d7
Michał Sitko <msitko@msitko.pl>
2026-01-29 09:36:16 +0100

    Update deps

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
 	lazy val cats  = "org.typelevel" %% "cats-core" % "2.13.0"
-	lazy val munit = "org.scalameta" %% "munit" % "1.1.0" % Test
+	lazy val munit = "org.scalameta" %% "munit" % "1.2.1" % Test
 
 	lazy val compileDeps = Seq(cats)
 	lazy val testDeps    = Seq(munit)
